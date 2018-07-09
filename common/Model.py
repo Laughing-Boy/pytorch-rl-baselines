0618ba05af5b1913231640d22f77dab03a941ac9
Jyothir Aditya <jyothir_aditya@outlook.com>
2018-07-09 12:22:17 +0530

    first commit with realvent comments

---
+++ common/Model.py
@@ -0,0 +1,78 @@
+
+import torch as th
+from torch import nn
+
+
+class ActorNetwork(nn.Module):
+    """
+    A network for actor
+    Args :
+    state_dim : last dimensions of state
+    hidden_size : dimension of hidden layer
+    output_act : activation function for the output
+
+    Returns : probabilities for various actions
+    """
+
+    def __init__(self, state_dim, hidden_size, output_size, output_act):
+        super(ActorNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, output_size)
+        self.output_act = output_act
+
+    def __call__(self, state):
+        out = nn.functional.relu(self.fc1(state))
+        out = nn.functional.relu(self.fc2(out))
+        out = self.output_act(self.fc3(out))
+        return out
+
+
+class CriticNetwork(nn.Module):
+    """
+    A network for critic.
+    Same as actor. But in hiden layer, We are concating the output with action along 2nd dimension.
+    Args :
+    state_dim : last dimensions of state
+    hidden_size : dimension of hidden layer
+    output_act : activation function for the output (by default size 1, as critic gives score to given actor's actions)
+
+    Returns : probabilities for various actions
+    """
+
+    def __init__(self, state_dim, action_dim, hidden_size, output_size=1):
+
+        super(CriticNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_size)
+        self.fc2 = nn.Linear(hidden_size + action_dim, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, output_size)
+
+    def __call__(self, state, action):
+        out = nn.functional.relu(self.fc1(state))
+        out = th.cat([out, action], 1)
+        out = nn.functional.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
+
+
+class ActorCriticNetwork(nn.Module):
+    """
+    An actor-critic network that shared lower-layer representations but
+    have distinct output layers
+    """
+
+    def __init__(self, state_dim, action_dim, hidden_size,
+                 actor_output_act, critic_output_size=1):
+        super(ActorCriticNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.actor_linear = nn.Linear(hidden_size, action_dim)
+        self.critic_linear = nn.Linear(hidden_size, critic_output_size)
+        self.actor_output_act = actor_output_act
+
+    def __call__(self, state):
+        out = nn.functional.relu(self.fc1(state))
+        out = nn.functional.relu(self.fc2(out))
+        act = self.actor_output_act(self.actor_linear(out))
+        val = self.critic_linear(out)
+        return act, val
